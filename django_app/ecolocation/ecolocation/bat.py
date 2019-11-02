e9ce64a5f9c79d24f079a12d96693f6d7c081d27
Jonathan Lee <jonathan.h.lee@duke.edu>
2019-11-02 17:07:37 -0400

    Fixed bat_types and bat.py

--- django_app/ecolocation/ecolocation/bat.py
+++
@@ -1,7 +0,0 @@
-class Bat:
-    """A simple example class"""
-    class Bat(models.Model):
-    Name = models.ForeignKey(Question, on_delete=models.CASCADE)
-    Frequency = models.CharField(max_length=200)
-    Proability = models.IntegerField(default=0)
-    Image = models.ImageField
