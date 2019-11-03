64ea6f45fecd5eb0a8493cdd6a7b4243c09672fe
Jonathan Lee <jonathan.h.lee@duke.edu>
2019-11-03 13:18:53 -0500

    sadjdw

--- bat.py
+++
@@ -1,7 +0,0 @@
-class Bat:
-    def __init__(self, tp, freq, prob, hc, img):
-        self.type = tp
-        self.frequency = freq
-        self.probability = prob
-        self.hexcolor = hc
-        self.imgurl = img
--- batdjango.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Bat(models.Model):
-    name = models.ForeignKey(Question, on_delete=models.CASCADE)
-    frequency = models.CharField(max_length=200)
-    probability = models.FloatField(default=0)
-    image = models.CharField(max_length=1000)
